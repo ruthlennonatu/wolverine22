bb5891d07bb056e5c74d2bf9e80c52c755c30108
l00162530 <l00162530@atu.ie>
2022-11-22 21:51:23 +0000

    Add node.js application with dependencies for SBOM #139

---
+++ base_templates/Node_JS_(JavaScript)/logger.js
@@ -0,0 +1,23 @@
+
+//adding winston logging library for security analysis
+//This file has some dependencies for security analysis
+//Software bill of material will be generated and a suitable tool
+
+'use strict';
+
+const winston = require('winston');
+const tsFormat = () => (new Date()).toLocaleTimeString();
+
+let logger = winston.createLogger({ 
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      level: 'info'
+    }),
+  ],
+  exitOnError: false
+});
+
+module.exports = logger;
